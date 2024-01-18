92d60c17d5403baa6aa2f1fe12607c1b3212788b
GAMGDOG <chltjdwo98@gmail.com>
2024-01-18 20:28:16 +0900

    [Feat] Add Option LobbyScene

--- Assets/Scripts/UI/SceneUI/UI_Scene_Lobby.cs
+++ Assets/Scripts/UI/SceneUI/UI_Scene_Lobby.cs
@@ -1,11 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UI_Scene_Lobby : MonoBehaviour
 {
-    private void Start()
-    {
-        
-    }
+
 }
