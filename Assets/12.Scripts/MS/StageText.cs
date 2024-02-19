71f79f638b3b9c752911c6de6b8f9fc5bf1aabdd
alstn1234 <rhalstn8549@gmail.com>
2024-02-19 14:23:04 +0900

    [Fix] Quest, Stage
    - 악기 오브젝트 위에 스테이지 텍스트 추가
    - 퀘스트 창 해상도에 맞게 수정

---
+++ Assets/12.Scripts/MS/StageText.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageText : MonoBehaviour
+{
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Managers.Player != null)
+        {
+            transform.LookAt(Camera.main.transform);
+            transform.Rotate(new Vector3(0, 180f, 0));
+        }
+    }
+}
