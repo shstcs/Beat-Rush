a3985616faf11bc163de99422abbb7cb6bfe3b56
GAMGDOG <chltjdwo98@gmail.com>
2024-02-19 20:32:18 +0900

    [Feat] Tutorial Fix, Add Stage3 warningpanel, Rank Algorythm fix

---
+++ Assets/12.Scripts/UI/PopupUI/UI_Popup_StageWanring.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Popup_StageWanring : MonoBehaviour, IPopup
+{
+    private void Start()
+    {
+        Managers.Sound.StopBGM();
+        Managers.Popup.CurrentPopup = this;
+    }
+
+    private void OnDisable()
+    {
+        Managers.Sound.DelayedPlayBGM(BGM.Stage3, 32.5f / (Managers.Game.noteSpeed[3] * Managers.Game.speedModifier));        //음악 재생
+        Managers.Popup.CurrentPopup = null;
+    }
+
+    public void OffPopup()
+    {
+
+    }
+}
