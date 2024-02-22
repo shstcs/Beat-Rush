0616d101e121a2332c1dd61845c3a5878c25c7c5
shstcs <shstcs@naver.com>
2024-02-22 10:43:12 +0900

    [Fix] LobbyArrow
    - 스테이지별로 달라지게 수정

--- Assets/12.Scripts/MH/LobbyArrow.cs
+++ Assets/12.Scripts/MH/LobbyArrow.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 
 public class LobbyArrow : MonoBehaviour
 {
-    public GameObject target;
+    public GameObject Stage1Object;
+    public GameObject Stage2Object;
+    public GameObject Stage3Object;
     private Vector3 dir;
 
     private void Update()
     {
+        if (Managers.Game.currentStage > 3) Destroy(this);
+
+        GameObject target = Managers.Game.currentStage switch
+        {
+            1 => Stage1Object,
+            2 => Stage2Object,
+            3 => Stage3Object,
+        };
         dir = target.transform.position - transform.position;
         transform.rotation = Quaternion.LookRotation(dir, transform.up);
     }
