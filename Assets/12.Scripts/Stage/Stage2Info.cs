f02a6ec618a973aa39e21322d6780bba3e5ca086
GAMGDOG <chltjdwo98@gmail.com>
2024-02-28 20:50:18 +0900

    [Fix]

--- Assets/12.Scripts/Stage/Stage2Info.cs
+++ Assets/12.Scripts/Stage/Stage2Info.cs
@@ -9,7 +9,7 @@ public class Stage2Info : StageInfo
         bpm = 99f;
         noteDistance = 8f;
         noteSpeed = 13.2f;
-        StageStartDelay = -2.5f;
+        StageStartDelay = 0f;
         PatternLength = 128f;
         PatternCount = 9;
         StageNotePos = new Vector3(40, 1, 42.5f);
